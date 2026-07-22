42bef06e80d375a1d7ba3968ccadebf393d790ec
Ender Jimenez <enderst2025@gmail.com>
2026-07-22 16:33:53 -0500

    Subo mi primer trabajo de ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+ <main>
+ <h1>Hola SENA </h1>
+ <p>Mi nombre es Ender Jimenez, y esta es mi primera clase de React.js </p>
+ </main>)
+}
