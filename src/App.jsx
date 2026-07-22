a1d8c041d4a6c04dfb93e99ba6ad19ac07cc530c
Ender Jimenez <enderst2025@gmail.com>
2026-07-22 17:17:42 -0500

    Personalización del componente App

--- src/App.jsx
+++ src/App.jsx
@@ -4,5 +4,7 @@ return (
  <main>
  <h1>Hola SENA </h1>
  <p>Mi nombre es Ender Jimenez, y esta es mi primera clase de React.js </p>
+ <p>Fecha y Hora: {fecha}</p>
+ <p>Durante mi transcurso de este curso espero poder aprender todal cas competencias que tenga en la agenda de aprenzaje y ser capaz de aplicar esos conocimientos</p>
  </main>)
 }
