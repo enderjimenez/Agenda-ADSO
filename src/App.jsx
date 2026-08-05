ff15c538f5c4c0b4c963d1d48661d3c0d6856d72
enderjimenez <enderst2025@gmail.com>
2026-08-05 15:59:00 -0500

    Se trabajo en la primera version de agenda-adso agregando 5 contactos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,70 @@
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+
 export default function App() {
-const fecha = new Date().toLocaleString();
-return (
- <main>
- <h1>Hola SENA </h1>
- <p>Mi nombre es Ender Jimenez, y esta es mi primera clase de React.js </p>
- <p>Fecha y Hora: {fecha}</p>
- <p>Durante mi transcurso de este curso espero poder aprender todal cas competencias que tenga en la agenda de aprenzaje y ser capaz de aplicar esos conocimientos</p>
- </main>)
+  // Esta es nuestra "base de datos" inicial quemada en el código
+  const contactos = [
+    {
+      id: 1,
+      nombre: "Carolina Pérez",
+      telefono: "300 123 4567",
+      correo: "carolina@sena.edu.co",
+      etiqueta: "Compañera",
+    },
+    {
+      id: 2,
+      nombre: "Juan Díaz",
+      telefono: "301 987 6543",
+      correo: "juan@sena.edu.co",
+      etiqueta: "Instructor",
+    },
+    {
+      id: 3,
+      nombre: "Luisa Martínez",
+      telefono: "320 555 7788",
+      correo: "luisa@sena.edu.co",
+      etiqueta: "Cliente",
+    },
+
+    {
+        id: 4,
+        nombre: "Sebastián Serna",
+        telefono: "314 563 2456",
+        correo: "sebastian@sena.edu.co",
+        etiqueta: "Compañero",
+
+    },
+
+    {
+        id: 5,
+        nombre: "Andrés Deossa",
+        telefono: "334 533 2446",
+        correo: "andres@sena.edu.co",
+        etiqueta: "Compañero",
+
+    },
+  ];
+
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO 📒</h1>
+
+      <p className="app-subtitle">Contactos guardados</p>
+
+      {/* Recorremos el arreglo contactos y pintamos una tarjeta por cada uno */}
+      {contactos.map((c) => (
+        <ContactoCard
+          key={c.id} // key única para React
+          nombre={c.nombre} // prop nombre
+          telefono={c.telefono} // prop telefono
+          correo={c.correo} // prop correo
+          etiqueta={c.etiqueta} // prop etiqueta (Cliente, Instructor, etc.)
+        />
+      ))}
+
+      <p className="app-nota">
+        (Versión 0.1 - solo lectura, sin agregar ni editar todavía)
+      </p>
+    </main>
+  );
 }
